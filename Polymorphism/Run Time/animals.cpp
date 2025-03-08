d77dfdd47e7549dfd6aa7bac5dd8db341f25d3db
aswathcm29 <aswathcm29@gmail.com>
2025-03-08 09:40:13 +0530

    Added few implementation of runtime polymorphism

--- Polymorphism/Run Time/animals.cpp
+++ Polymorphism/Run Time/animals.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 //Run time Polymorphism
-
 class Animal{
     public:
     virtual void sound(){
@@ -13,13 +12,40 @@ class Animal{
 class Dog : public Animal{
     public:
 
-    void sound () {
-        cout<<"Dog has a sound"<<endl;
+    void sound () override{
+        cout<<"Dog barks"<<endl;
+    }
+};
+
+class Cat : public Animal{
+    public:
+
+    void sound() override {
+         cout<<"Cat meows"<<endl;
     }
 };
 
 int main(){
-    Dog sonie;
-    Animal* sdog= &sonie;
-    sdog->sound();
+    Dog sonie;    //Calls dound of dog directly
+    sonie.sound();
+
+    Cat kitty;
+    kitty.sound();
+
+    vector<Animal>animals;   // gives output as the sound() of base class
+    animals.push_back(Dog());
+    animals.push_back(Cat());
+    for (auto x:animals){
+        x.sound();
+    }
+
+
+    vector<Animal*>animals2;   // gives output as the sound() of the derived class using base class pointer
+    animals2.push_back(new Dog());
+    animals2.push_back(new Cat());
+    for (auto x:animals2){
+        x->sound();
+    }
+
+    
 }
