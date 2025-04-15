65ed413d4d46b4e9fc4b66969d0c46a46b0ee3b7
aswathcm29 <aswathcm29@gmail.com>
2025-04-15 13:03:03 +0530

    Added producer consumer problem in multithreading

---
+++ Multithreading/Problems/producer-consumer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+const int MAX_SIZE = 5;
+int buffer[MAX_SIZE];
+int count = 0;
+
+bool isProducing = true; 
+
+void producer() {
+    int value = 0;
+    while (value<10) {
+        if (isProducing && count<MAX_SIZE) {
+            buffer[count++] = value;
+            cout << "Produced: " << value << endl;
+            value++;
+            isProducing = false;
+        }
+        this_thread::sleep_for(chrono::milliseconds(100)); 
+    }
+}
+
+void consumer() {
+    int consumed=0;
+    while (consumed<10) {
+        if (!isProducing && count > 0) {
+            int value = buffer[--count];
+            cout << "Consumed: " << value << endl;
+            consumed++;
+            isProducing = true; 
+        }
+        this_thread::sleep_for(chrono::milliseconds(100)); // simulate delay
+    }
+}
+
+int main() {
+    thread t1(producer);
+    thread t2(consumer);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
